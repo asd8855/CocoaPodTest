48adee7ca1080b9ca32da4d00009fa2fd7073319
asd <asd13148866@sina.com>
2017-12-13 17:22:52 +0800

    CocoaPodTest

---
+++ CocoaPodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoaPodTest
+//
+//  Created by libo on 2017/12/13.
+//  Copyright © 2017年 蝉鸣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocoaPodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoaPodTest
+//
+//  Created by libo on 2017/12/13.
+//  Copyright © 2017年 蝉鸣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
